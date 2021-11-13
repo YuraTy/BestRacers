4e5e4282033c7f4ea6cd78d1b9926c8905ac60a4
Yura <odnorog.yu@gmail.com>
2021-11-13 19:29:44 +0200

    working file reading

--- src/main/java/com/foxminded/bestRacers/BestRacers.java
+++ src/main/java/com/foxminded/bestRacers/BestRacers.java
@@ -1,4 +1,22 @@
 package com.foxminded.bestRacers;
 
+import com.foxminded.readingData.ReadingData;
+
+import java.io.IOException;
+import java.time.LocalTime;
+import java.util.HashMap;
+import java.util.Map;
+
 public class BestRacers {
+    public static void main(String[] args) throws IOException {
+        ReadingData readingData = new ReadingData();
+        Map<String,String > abbreviationMap = new HashMap<>(readingData.abbreviationParticipants("abbreviations.txt"));
+        Map<String, LocalTime> startMap = new HashMap<>(readingData.timeParticipants("start.log"));
+        Map<String,LocalTime > endMap = new HashMap<>(readingData.timeParticipants("end.log"));
+
+        System.out.println(abbreviationMap);
+        System.out.println(startMap);
+        System.out.println(endMap);
+
+    }
 }
