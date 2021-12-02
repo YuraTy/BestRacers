20163c7bc63c3d9ea1fc1dc81a0b9489e65dd1be
Yura Odnorog <odnorog.yu@gmail.com>
2021-12-02 17:19:45 +0200

    refactoring

--- src/main/java/com/foxminded/timeformatter/TimeFormatter.java
+++ src/main/java/com/foxminded/timeformatter/TimeFormatter.java
@@ -1,12 +1,9 @@
 package com.foxminded.timeformatter;
 
-import com.foxminded.racerdao.RacerDao;
 import com.foxminded.racerservice.RacerService;
 import org.apache.commons.lang3.time.DurationFormatUtils;
 
 import java.time.Duration;
-import java.util.Optional;
-import java.util.StringJoiner;
 import java.util.stream.Collectors;
 
 public class TimeFormatter {
@@ -16,23 +13,15 @@ public class TimeFormatter {
 
     public String resultOutput() {
         return racerService.bestCircle().stream()
-                .map(p -> String.format("%-1s%s%s", searchObject(p.getName()).get(), timeFormatting(p.getTravelTime()),delimiterTop15(numberRacer)))
+                .peek(p -> numberRacer += 1)
+                .map(p -> String.format("%-3d%-18s|%-26s|%s%s", numberRacer, p.getNameSurname(), p.getCar(), timeFormatting(p.getTravelTime()), delimiterTop15(numberRacer)))
                 .collect(Collectors.joining("\n"));
     }
 
-    private Optional<String> searchObject(String name) {
-        numberRacer += 1;
-        return racerService.bestCircle().stream()
-                .filter(p -> p.getName().equals(name))
-                .map(p -> String.format("%-3d%-18s|%-26s|", numberRacer, p.getNameSurname(), p.getCar()))
-                .findFirst();
-    }
-
-    private String delimiterTop15 (int numberRacer){
-        if (numberRacer == 15){
+    private String delimiterTop15(int numberRacer) {
+        if (numberRacer == 15) {
             return "\n" + "---------------------------------------------------------";
-        }
-        else return "";
+        } else return "";
     }
 
     private String timeFormatting(Duration travelTime) {
